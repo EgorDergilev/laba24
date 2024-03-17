ba0df374ef2c7354c191b75f1c771b47e441182a
egoroabs323 <egoroleinik19@gmail.com>
2024-03-17 22:32:22 +0700

    2laba

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL,"Russian");
+    string input, output;                            //переменные ввод/вывод
+    cout << "Введите строку: ";
+    getline(cin, input);
+    
+    for(char c : input) {                            // Проходим по каждому символу во введенной строке               
+        if(isspace(c) || isalnum(c) || c == ' ') {   // Проверяем является ли символ допустимым 
+            output += c;
+        }
+    }
+    
+    cout << "Результат: " << output << endl;
+    
+    return 0;
+}
